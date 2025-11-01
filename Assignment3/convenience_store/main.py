d8e51811f895b81211c10f1e94fd5649eb6493d9
dinhnam811 <dnamnguyen2005@gmail.com>
2025-11-02 01:36:33 +1100

    Update local host port

--- Assignment3/convenience_store/main.py
+++ Assignment3/convenience_store/main.py
@@ -341,4 +341,4 @@ if __name__ == "__main__":
     print("Starting server...")
     print("Once started, open your browser to: http://localhost:8000")
     print("=" * 60)
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
